66c053bb92943ad58779148a906bf49d3f9e391e
twmht <qrnnis2623891@gmail.com>
2017-04-08 00:19:14 +0800

    bump to 0.6.2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ mod1 = Extension(
 
 setup(
     name="python-rocksdb",
-    version='0.6.1',
+    version='0.6.2',
     description="Python bindings for RocksDB",
     keywords='rocksdb',
     author='Ming Hsuan Tu',
